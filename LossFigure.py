6c25d6e09079e7a17d7f2af4af7dc6e249440c59
HuangTao <3274436351@qq.com>
2017-12-01 13:49:36 +0800

    update lossFigure.py

--- LossFigure.py
+++ LossFigure.py
@@ -5,7 +5,7 @@ from mpl_toolkits.axes_grid1 import host_subplot
 
 # read the log file
 
-file = 'loss_log.txt'
+file = './checkpoints/experiment_name/loss_log.txt'
 
 '''
 读取数据
@@ -20,31 +20,36 @@ def get_loss(dir):
             train_loss.append(arr[0].strip(' ')[6:])
             # print(train_loss)
 
+    data_numpy = len(train_loss)
+
     fp.close()
 
-    return train_loss
+    return train_loss,data_numpy
 
 '''
 画图
 '''
-def draw_figure():
+def draw_loss_figure(save_id):
     host = host_subplot(1,1,1)#分几个图，选择第几个图
     plt.subplots_adjust(right=0.8)# 限定右边界
 
     host.set_xlabel("epoch")
     host.set_ylabel("D_A loss")
 
-    p1, = host.plot(get_loss(file), label="train cycleGANs D_A loss")
+    train_loss,data_numpy = get_loss(file)
+
+    p1, = host.plot(train_loss, label="train cycleGANs D_A loss")
 
     host.legend(loc=1)#标签距离边界位置
 
     host.axis["left"].label.set_color(p1.get_color())
 
-    host.set_xlim([-1, 50])#x轴范围
-    host.set_ylim([0.,0.8])#y轴范围
+    host.set_xlim([-1, data_numpy])#x轴范围
+    host.set_ylim([0.,1])#y轴范围
 
     plt.draw()
-    plt.show()
+    plt.savefig('./lossfigure/%d.png'%save_id)
+    #plt.show()
 
 if __name__ == '__main__':
-    draw_figure()
+    draw_loss_figure(1)
--- train.py
+++ train.py
@@ -3,18 +3,19 @@ from options.train_options import TrainOptions
 from data.data_loader import CreateDataLoader
 from models.models import create_model
 from utilSet.visualizer import Visualizer
+from LossFigure import draw_loss_figure
 
 opt = TrainOptions().parse()
 data_loader = CreateDataLoader(opt)
 dataset = data_loader.load_data()
+dataset_size = len(data_loader)
 
 model = create_model(opt)
 visualizer = Visualizer(opt)
 
-
 if __name__ == '__main__':
     total_steps = 0
-    for epoch in range(1, 201):#训练200轮
+    for epoch in range(1, 201):
         epoch_start_time = time.time()
         epoch_iter = 0
 
@@ -30,10 +31,14 @@ if __name__ == '__main__':
                 save_result = total_steps % 1000 == 0
                 visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
 
-                errors = model.get_current_errors()
-                t = (time.time() - iter_start_time) / opt.batchSize
-                visualizer.print_current_errors(epoch, epoch_iter, errors, t)
-                # epoch 训练轮数  iters 训练图片数量，
+            #每次运行都计算loss值
+            errors = model.get_current_errors()
+            t = (time.time() - iter_start_time) / opt.batchSize
+            visualizer.print_current_errors(epoch, epoch_iter, errors, t)
+            # epoch 训练轮数  iters 训练图片数量，
+
+            if total_steps % dataset_size == 0:
+                draw_loss_figure(total_steps/dataset_size)##以训练轮数命名
 
             if total_steps % 5000 == 0:
                 #每5000张图片保存一次，此处共2200张图片，故每两轮多600张时会保存一次
